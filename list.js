8194e5f2eecf58a3bda3a3202fb663ec0f61fff4
dsignmind <dsignmind@gmail.com>
2018-12-24 13:41:55 -0800

    add alert for empty input.

--- list.js
+++ list.js
@@ -13,39 +13,18 @@ function populateList() {
         newItem(listItem);
     });
 }
-
-// var btn = document.getElementById('addItemButton');
-// btn.onclick = function() {
-//     let itemText = document.getElementById('inputText').value
-//     if (itemText === "") {
-//         <div class="alert alert-danger" role="alert">
-//             You didn't enter anything!
-//         </div>
-//     } else {
-//         newItem(itemText);
-//         listItems.push(itemText);
-//         document.getElementById('inputText').value = "";
-//     }
-// }
+function closeAlert() {
+    var x = document.getElementById('alert-div');
+    x.style.display = "none";
+}
 function addItem() {
     let itemText = document.getElementById('inputText').value
+    var x = document.getElementById('alert-div');
     if (itemText === "") {
-        // alert('No Input');
-        // console.log('I am getting called');
-        // var x = document.getElementById('alert-div');
-        // if (x.style.display === "none") {
-        //     x.style.display = "block";
-        // } else {
-        //     x.style.display = "none";
-        // }
-        // <div id="alert-div" class="alert alert-danger" role="alert" style="display: none">
-        //     You didn't enter anything!
-        //     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-        //         <span aria-hidden="true">&times;</span>
-        //     </button>
-        // </div>
-        // document.getElementById('alert-div').show()
+        console.log('I am getting called');
+        x.style.display = "block";
     } else {
+        x.style.display = "none";
         newItem(itemText);
         listItems.push(itemText);
         document.getElementById('inputText').value = "";
